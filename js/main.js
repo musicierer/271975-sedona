200d1683a1dd79d57bd1a60a21b166d0381c4407
musicierer <musicierer@gmail.com>
2016-11-21 16:33:25 +0200

    Исправления по первой странице

--- js/main.js
+++ js/main.js
@@ -1,8 +1,10 @@
-        var button = document.querySelector(".fined-hotel-btn");
-    	var popup = document.querySelector(".choose-hotel");
-    	var open = document.querySelector(".choose-hotel-close");
+var button = document.querySelector(".fined-hotel-btn");
+var popup = document.querySelector(".choose-hotel");
+var open = document.querySelector(".choose-hotel-close");
 
-    	button.addEventListener("click", function(event) {
-    	event.preventDefault();
-    	popup.classList.toggle("choose-hotel-close");
-    	});
+button.addEventListener("click", function (event) {
+    event.preventDefault();
+    popup.classList.toggle("choose-hotel-close");
+});
+
+popup.classList.remove('choose-hotel-close');
