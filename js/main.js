88dc6f9f1c20a66bb5ad838ccc49475ef6aca8ea
musicierer <musicierer@gmail.com>
2016-11-21 21:32:10 +0200

    2 пересдача

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ var button = document.querySelector(".fined-hotel-btn");
 var popup = document.querySelector(".choose-hotel");
 var open = document.querySelector(".choose-hotel-close");
 
-button.addEventListener("click", function (event) {
+button.addEventListener("click", function(event) {
     event.preventDefault();
     popup.classList.toggle("choose-hotel-close");
 });
